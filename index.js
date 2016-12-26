3e00714740bac567745585043152db2ef4887005
Andrew Liu <aliu1@umd.edu>
2016-12-26 00:36:49 -0800

    Add testing code and babel-node for dev

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
   require('./dist/server.bundle.js');
 } else {
-  // TODO: polyfill server code for dev
-  require('./dist/server.bundle.js');
+  require('./server/server.js');
 }
