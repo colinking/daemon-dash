c8053b7287e4c61dc67b55e734ccc34754e2c8a8
Andrew Liu <aliu1@umd.edu>
2016-12-28 00:06:44 -0800

    Allow configurable port

--- server/server.js
+++ server/server.js
@@ -4,15 +4,16 @@ import path from 'path';
 const app = express();
 const Console = console;
 
-app.use(express.static(path.join(__dirname, '../dist')));
-
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => {
-  Console.log('Server listening on port 3000.');
+app.listen(app.get('port'), () => {
+  Console.log('Server listening on port %d in %s mode.',
+      app.get('port'), process.env.NODE_ENV);
 });
