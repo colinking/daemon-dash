e7cd813cc33bb19df4c97cd6a108bf00a6cd7bd0
Andrew Liu <aliu1@umd.edu>
2016-12-28 16:21:54 -0800

    Restrict scope of css modules

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     }, {
       test: /\.css$/,
       include: /client/,
-      loaders: ['style', 'css'],
+      loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
     }],
   },
 };
