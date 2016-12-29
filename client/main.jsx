7921546c505d723deff23df510e7a0a81a4defa2
Andrew Liu <aliu1@umd.edu>
2016-12-28 22:18:37 -0800

    Add css extract text plugin and refactor eslint coverage

--- client/main.jsx
+++ client/main.jsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 
 import styles from './main.css';
 
-ReactDOM.render((<div><p>This is a paragraph generated via React.</p></div>),
-    document.getElementById('root'));
+ReactDOM.render((
+  <div className={styles.reactParagraph}>
+    <p>This is a paragraph generated via React.</p>
+  </div>
+), document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'client/main.jsx'),
@@ -19,7 +20,12 @@ module.exports = {
     }, {
       test: /\.css$/,
       include: /client/,
-      loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+      loader: ExtractTextPlugin.extract('style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
     }],
   },
+
+  plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+  ],
 };
