7f9194f1539e21448542b5c3e2e76ed1ea19efd8
Andrew Liu <aliu1@umd.edu>
2017-01-01 14:51:56 -0800

    Combine client and server webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const fs = require('fs');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+const config = {
   entry: path.resolve(__dirname, 'client/main.jsx'),
 
   output: {
@@ -33,3 +34,43 @@ module.exports = {
     new ExtractTextPlugin('style.css', { allChunks: true }),
   ],
 };
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+const serverConfig = {
+  entry: path.resolve(__dirname, 'server/server.js'),
+  target: 'node',
+  externals: nodeModules,
+
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'server.bundle.js',
+  },
+
+  module: {
+    loaders: [{
+      loader: 'json-loader',
+      include: /server/,
+      test: /\.json$/,
+    }, {
+      test: /\.js$/,
+      include: /server/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015'],
+      },
+    }],
+  },
+};
+
+module.exports = [config, serverConfig];
