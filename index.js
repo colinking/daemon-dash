fe6ba7546ca9fbc94d862504ae97c1558f4fc80d
Andrew Liu <aliu1@umd.edu>
2017-01-02 21:54:06 -0800

    Defensively make non-development environments production

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
  * Run the bundled server code in production, and run the runtime-generated
  * server code in development.
  */
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
+  process.env.NODE_ENV = 'production';
   require('./dist/server.bundle.js');
 } else {
   require('css-modules-require-hook/preset');
