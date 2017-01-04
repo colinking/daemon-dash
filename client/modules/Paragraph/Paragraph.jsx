30304de7185f3b1d8486977cec31a8242c1d7dab
Andrew Liu <aliu1@umd.edu>
2017-01-04 13:07:39 -0800

    Add semantic ui react component example

--- client/modules/Paragraph/Paragraph.jsx
+++ client/modules/Paragraph/Paragraph.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
 
 import styles from './Paragraph.scss';
 
 export default function Paragraph() {
   return (
     <div className={styles.reactParagraph}>
+      <Button>This is a Semantic Ui Button.</Button>
       <p>This is a paragraph generated via React.</p>
     </div>
   );
