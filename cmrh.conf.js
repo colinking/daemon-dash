70309cb70e782d3cdc73aaa286606d471cf18afa
Andrew Liu <aliu1@umd.edu>
2017-01-04 17:55:08 -0800

    Fix scss require hook style

--- cmrh.conf.js
+++ cmrh.conf.js
@@ -3,5 +3,5 @@ const sass = require('node-sass');
 module.exports = {
   generateScopedName: '[name]__[local]___[hash:base64:5]',
   extensions: ['.css', '.scss'],
-  preprocessCss: (data, filename) => sass.renderSync({ data }).css,
+  preprocessCss: data => sass.renderSync({ data }).css,
 };
