9b08ab88e84f30f33f1197a865f393421e3a2d22
Darshan <dhshah11@terpmail.umd.edu>
2017-01-28 16:02:27 -0500

    Student Professor editor worksish

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, IndexRedirect } from 'react-router';
 
-import Paragraph from './modules/Paragraph/Paragraph';
+import ProfessorView from './modules/ProfessorView/ProfessorView';
+import studentView from './modules/StudentView/StudentView';
 import NotFoundPage from './modules/NotFoundPage';
+import ErrorPage from './modules/ErrorPage';
 import Layout from './modules/Layout';
 
 export default (
   <Route path="/" component={Layout}>
-    <IndexRoute component={Paragraph} />
+    <IndexRedirect to="/professorView" />
+    <Route path="/professorView" component={ProfessorView} />
+    <Route path="/studentView" component={studentView} />
     <Route path="*" component={NotFoundPage} />
   </Route>
 );
