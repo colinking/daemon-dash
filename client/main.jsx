328cd2ff993e3785eed36510a2782a30ee6f6931
Andrew Liu <aliu1@umd.edu>
2017-01-28 19:45:50 -0800

    Add front end video recording

--- client/main.jsx
+++ client/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import IO from 'socket.io-client';
 
 import './global.scss';
 
 import routes from './routes';
-
-const socket = IO();
-
-
-socket.on('connected', (data) => {
-  console.log(data); // eslint-disable-line no-console
-});
 
 ReactDOM.render((
   <Router history={browserHistory}>
--- client/modules/Stream/RecordingPage.jsx
+++ client/modules/Stream/RecordingPage.jsx
@@ -1,8 +1,88 @@
 import React from 'react';
 import $ from 'jquery';
 import io from 'socket.io-client';
+import { Button } from 'semantic-ui-react';
 
 export default class RecordingPage extends React.Component {
+
+  constructor() {
+    super();
+    this.isRecording = false;
+    this.mediaStream = null;
+    this.recordAudio;
+    this.recordVideo;
+    this.socket = io();
+
+    this.handleStartRecording = this.handleStartRecording.bind(this);
+    this.handleStopRecording = this.handleStopRecording.bind(this);
+    console.log('binding');
+  }
+
+  handleStartRecording() {
+    console.log('handle start recording');
+    if (this.isRecording) {
+      return;
+    }
+    this.isRecording = true;
+    navigator.getUserMedia({
+      audio: true,
+      video: true
+    }, (stream) => {
+      this.mediaStream = stream;
+      this.recordAudio = RecordRTC(stream, {
+        onAudioProcessStarted: () => {
+          this.recordVideo.startRecording();
+          this.cameraPreview = $('#camera-preview')[0];
+          this.cameraPreview.src = window.URL.createObjectURL(stream);
+          this.cameraPreview.play();
+          this.cameraPreview.muted = true;
+          this.cameraPreview.controls = false;
+        }
+      });
+      this.recordVideo = RecordRTC(stream, {
+        type: 'video'
+      });
+      this.recordAudio.startRecording();
+    }, (error) => {
+      alert(JSON.stringify(error));
+    });
+  }
+
+  handleStopRecording() {
+    console.log('handle stop recording');
+    if (!this.isRecording) {
+      return;
+    }
+    this.isRecording = false;
+
+    this.recordAudio.stopRecording(() => {
+      // stop video recorder
+      this.recordVideo.stopRecording(() => {
+        // get audio data-URL
+        this.recordAudio.getDataURL((audioDataURL) => {
+          // get video data-URL
+          this.recordVideo.getDataURL((videoDataURL) => {
+            let files = {
+              audio: {
+                type: this.recordAudio.getBlob().type || 'audio/wav',
+                dataURL: audioDataURL
+              },
+              video: {
+                type: this.recordVideo.getBlob().type || 'video/webm',
+                dataURL: videoDataURL
+              }
+            };
+            this.socket.emit('recorded video', files);
+            if (this.mediaStream) {
+              this.mediaStream.stop();
+            }
+          });
+        });
+        this.cameraPreview.src = '';
+        console.log('src should be zero');
+      });
+    });
+  }
 
   componentDidMount() {
     let mediaOptions = {
@@ -13,8 +93,7 @@ export default class RecordingPage extends React.Component {
     navigator.mediaDevices.enumerateDevices().then(function (devices) {
       let socket = io();
       for (var i = 0; i !== devices.length; ++i) {
-        var device = devices[i];
-        socket.emit('device found', device);
+        let device = devices[i];
         if (device.kind === 'videoinput') {
           if (device.label.indexOf('back') !== -1) {
             mediaOptions.video = {
@@ -24,28 +103,37 @@ export default class RecordingPage extends React.Component {
         }
       }
 
-    const webrtc = new SimpleWebRTC({
-      localVideoEl: 'localVideo',
-      autoRequestMedia: true,
-      media: mediaOptions,
-    });
-
-    webrtc.on('readyToCall', () => {
-      $.ajax({
-        type: 'POST',
-        url: '/nonce',
-        success: (data) => {
-          socket.emit('device found', 'joining room');
-          webrtc.joinRoom(data.nonce);
-        },
+     const webrtc = new SimpleWebRTC({
+        localVideoEl: 'localVideo',
+        autoRequestMedia: true,
+        media: mediaOptions,
       });
-    });
+
+      webrtc.on('readyToCall', () => {
+        $.ajax({
+          type: 'POST',
+          url: '/nonce',
+          success: (data) => {
+            socket.emit('device found', 'joining room');
+            webrtc.joinRoom(data.nonce);
+          },
+        });
+      });
     });
   }
 
   render() {
     return (
-      <video height="300" id="localVideo"></video>
+      <div>
+        <video height="700" id="localVideo"></video>
+        <br />
+        <br />
+        <Button onClick={this.handleStartRecording}>Start Recording</Button>
+        <Button onClick={this.handleStopRecording}>Stop Recording</Button>
+        <br />
+        <br />
+        <video id="camera-preview"></video>
+      </div>
     );
   }
 }
