a7b32b39e7a3f74d5b766ded9fa50eb8dcfb0b20
Colin King <colin.king.96@gmail.com>
2017-01-28 23:46:51 -0500

    Lecture setup

--- server/models/lecture.js
+++ server/models/lecture.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
-const DateOnly = require('mongoose-dateonly')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const lectureSchema = new Schema({
   live: { type: Boolean, required: true },
-  date: { type: DateOnly, default: new DateOnly() },
+  start: { type: Date, default: Date.now() },
   name: { type: String, required: true },
   code: [
     {
       timestamp: { type: Date, required: true },
-      text: { type: String, required: true },
+      text: { type: String, default: '' },
     },
   ],
 });
