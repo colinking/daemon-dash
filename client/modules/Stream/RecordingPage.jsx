68b0e603e9e954d4a5b071b3edaac9c74fd4331f
Andrew Liu <aliu1@umd.edu>
2017-01-28 21:49:24 -0800

    Change library and save videos as webm through http

--- client/modules/Stream/RecordingPage.jsx
+++ client/modules/Stream/RecordingPage.jsx
@@ -7,81 +7,85 @@ export default class RecordingPage extends React.Component {
 
   constructor() {
     super();
-    this.isRecording = false;
-    this.mediaStream = null;
-    this.recordAudio;
-    this.recordVideo;
     this.socket = io();
 
     this.handleStartRecording = this.handleStartRecording.bind(this);
     this.handleStopRecording = this.handleStopRecording.bind(this);
-    console.log('binding');
+    this.handleSave = this.handleSave.bind(this);
+    this.onMediaSuccess = this.onMediaSuccess.bind(this);
+  }
+
+	onMediaSuccess(stream) {
+		var video = document.createElement('video');
+		const videoWidth = 320;
+		const videoHeight = 240;
+		video = mergeProps(video, {
+			controls: true,
+			muted: true,
+			width: videoWidth,
+			height: videoHeight,
+			src: URL.createObjectURL(stream)
+		});
+		video.play();
+    const videosContainer = $('#vid-container')[0];
+		videosContainer.appendChild(video);
+		videosContainer.appendChild(document.createElement('hr'));
+		this.mediaRecorder = new MediaStreamRecorder(stream);
+		this.mediaRecorder.stream = stream;
+    //this.mediaRecorder.recorderType = MediaRecorderWrapper;
+		// don't force any mimeType; use above "recorderType" instead.
+		// mediaRecorder.mimeType = 'video/webm'; // video/webm or video/mp4
+    //this.mediaRecorder.mimeType = 'video/mp4';
+		this.mediaRecorder.videoWidth = videoWidth;
+		this.mediaRecorder.videoHeight = videoHeight;
+    //*
+		this.mediaRecorder.ondataavailable = function(blob) {
+			var a = document.createElement('a');
+			a.target = '_blank';
+			a.innerHTML = 'this is a video';
+			a.href = URL.createObjectURL(blob);
+			videosContainer.appendChild(a);
+			videosContainer.appendChild(document.createElement('hr'));
+		};
+    //*/
+    /*
+		var timeInterval = document.querySelector('#time-interval').value;
+		if (timeInterval) timeInterval = parseInt(timeInterval);
+		else timeInterval = 5 * 1000;
+		// get blob after specific time interval
+		mediaRecorder.start(timeInterval);
+		document.querySelector('#stop-recording').disabled = false;
+		document.querySelector('#pause-recording').disabled = false;
+		document.querySelector('#save-recording').disabled = false;
+    */
+		this.mediaRecorder.start(300000);
   }
 
   handleStartRecording() {
-    console.log('handle start recording');
-    if (this.isRecording) {
-      return;
-    }
-    this.isRecording = true;
-    navigator.getUserMedia({
+    // TODO disable start button
+    navigator.mediaDevices.getUserMedia({
       audio: true,
-      video: true
-    }, (stream) => {
-      this.mediaStream = stream;
-      this.recordAudio = RecordRTC(stream, {
-        onAudioProcessStarted: () => {
-          this.recordVideo.startRecording();
-          this.cameraPreview = $('#camera-preview')[0];
-          this.cameraPreview.src = window.URL.createObjectURL(stream);
-          this.cameraPreview.play();
-          this.cameraPreview.muted = true;
-          this.cameraPreview.controls = false;
-        }
-      });
-      this.recordVideo = RecordRTC(stream, {
-        type: 'video'
-      });
-      this.recordAudio.startRecording();
-    }, (error) => {
-      alert(JSON.stringify(error));
+      video: true,
+    }).then(this.onMediaSuccess).catch((e) => {
+      console.error('media error', e);
     });
   }
 
   handleStopRecording() {
-    console.log('handle stop recording');
-    if (!this.isRecording) {
-      return;
-    }
-    this.isRecording = false;
+    this.mediaRecorder.stop();
+    this.mediaRecorder.stream.stop();
+    /*
+    this.disabled = true;
+    mediaRecorder.stop();
+    mediaRecorder.stream.stop();
+    document.querySelector('#pause-recording').disabled = true;
+    document.querySelector('#start-recording').disabled = false;
+    */
+  }
 
-    this.recordAudio.stopRecording(() => {
-      // stop video recorder
-      this.recordVideo.stopRecording(() => {
-        // get audio data-URL
-        this.recordAudio.getDataURL((audioDataURL) => {
-          // get video data-URL
-          this.recordVideo.getDataURL((videoDataURL) => {
-            let files = {
-              audio: {
-                type: this.recordAudio.getBlob().type || 'audio/wav',
-                dataURL: audioDataURL
-              },
-              video: {
-                type: this.recordVideo.getBlob().type || 'video/webm',
-                dataURL: videoDataURL
-              }
-            };
-            this.socket.emit('recorded video', files);
-            if (this.mediaStream) {
-              this.mediaStream.stop();
-            }
-          });
-        });
-        this.cameraPreview.src = '';
-        console.log('src should be zero');
-      });
-    });
+  handleSave() {
+    console.log('handle save');
+    this.mediaRecorder.save();
   }
 
   componentDidMount() {
@@ -130,9 +134,10 @@ export default class RecordingPage extends React.Component {
         <br />
         <Button onClick={this.handleStartRecording}>Start Recording</Button>
         <Button onClick={this.handleStopRecording}>Stop Recording</Button>
+        <Button onClick={this.handleSave}>Save Recording</Button>
         <br />
         <br />
-        <video id="camera-preview"></video>
+        <div id="vid-container"></div>
       </div>
     );
   }
