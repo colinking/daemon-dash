e409a61ef66b7fdcdaf694fbc394a8e9a0196d55
Colin King <colin.king.96@gmail.com>
2017-01-28 23:05:59 -0500

    Updated lecture model

--- server/models/lecture.js
+++ server/models/lecture.js
@@ -6,6 +6,13 @@ const Schema = mongoose.Schema;
 const lectureSchema = new Schema({
   live: { type: Boolean, required: true },
   date: { type: DateOnly, default: new DateOnly() },
+  name: { type: String, required: true },
+  code: [
+    {
+      timestamp: { type: Date, required: true },
+      text: { type: String, required: true },
+    },
+  ],
 });
 
 module.exports = mongoose.model('lecture', lectureSchema);
